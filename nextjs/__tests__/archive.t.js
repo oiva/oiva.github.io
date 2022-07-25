9c298f8a57ace0d5f7052b7ba8a60e60cd821186
Oiva Eskola <oiva.eskola@gmail.com>
2022-07-25 21:46:03 +0300

    feed item compare test

--- nextjs/__tests__/archive.t.js
+++ nextjs/__tests__/archive.t.js
@@ -4,20 +4,20 @@ import { getPage } from 'next-page-tester'
 
 const testDirectory = join(process.cwd(), '__tests__')
 
-describe('Archive page', () => {
-  return
-  it('renders archive page', async () => {
-    const { serverRenderToString } = await getPage({
-      route: '/archive/elama/',
-    })
+// describe('Archive page', () => {
+//   return
+//   it('renders archive page', async () => {
+//     const { serverRenderToString } = await getPage({
+//       route: '/archive/elama/',
+//     })
 
-    const fullPath = join(testDirectory, `old-archive.html`)
-    let fileContents = fs.readFileSync(fullPath, 'utf8')
+//     const fullPath = join(testDirectory, `old-archive.html`)
+//     let fileContents = fs.readFileSync(fullPath, 'utf8')
 
-    fileContents = fileContents.replace(/\n\n/g, '')
-    let rendered = serverRenderToString()
-    rendered = rendered.html.replace(/\n\n/g, '')
+//     fileContents = fileContents.replace(/\n\n/g, '')
+//     let rendered = serverRenderToString()
+//     rendered = rendered.html.replace(/\n\n/g, '')
 
-    expect(rendered).toMatch(fileContents)
-  })
-})
+//     expect(rendered).toMatch(fileContents)
+//   })
+// })
--- nextjs/__tests__/feed.test.js
+++ nextjs/__tests__/feed.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import { join } from 'path'
 import { getServerSideProps } from '../pages/feed.xml'
-
+const util = require('util')
 const { XMLParser } = require('fast-xml-parser')
 
 const testDirectory = join(process.cwd(), '__tests__')
@@ -24,6 +24,23 @@ const getFeed = async () => {
   return { rendered, responseHeader, responseHeaderValue }
 }
 
+expect.extend({
+  toMatchField(received, oldValue, message) {
+    if (util.isDeepStrictEqual(received, oldValue)) {
+      return {
+        message: () =>
+          `${message}\n\nreceived: \x1b[32m${received}\n\n\x1b[37mexpected: \x1b[31m${oldValue}`,
+        pass: true,
+      }
+    }
+    return {
+      message: () =>
+        `${message}\n\nreceived: \x1b[32m${received}\n\n\x1b[37mexpected: \x1b[31m${oldValue}`,
+      pass: false,
+    }
+  },
+})
+
 describe('RSS Feed', () => {
   it('renders RSS feed with correct content type', async () => {
     const { responseHeader, responseHeaderValue } = await getFeed()
@@ -31,7 +48,7 @@ describe('RSS Feed', () => {
     expect(responseHeaderValue).toMatch('text/xml')
   })
 
-  it('new and old feeds match', async () => {
+  it('compare items', async () => {
     const fullPath = join(testDirectory, `old-feed.xml`)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
     const { rendered } = await getFeed()
@@ -39,13 +56,16 @@ describe('RSS Feed', () => {
 
     let docOld = parser.parse(fileContents)
     let docNew = parser.parse(rendered)
-    console.dir(docOld.rss.channel)
-    for (const el in docOld.rss.channel.item) {
-      const ret = expect(docOld.rss.channel.item[el]).toEqual(
-        docNew.rss.channel.item[el]
-      )
-      console.dir(ret)
+    for (const item in docOld.rss.channel.item) {
+      const oldItem = docOld.rss.channel.item[item]
+      for (const field in oldItem) {
+        const oldField = oldItem[field]
+        const newField = docNew.rss.channel.item[item][field]
+        expect(newField).toMatchField(
+          oldField,
+          `field '${field}' does not match`
+        )
+      }
     }
-    //expect(docNew).toEqual(docOld)
   })
 })
