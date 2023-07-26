00afd7e0b07a352d7c5e1d998d389bd38a3a00d4
Oiva Eskola <oiva.eskola@gmail.com>
2023-07-26 22:58:50 +0300

    config to output static files

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  output: 'export',
   assetPrefix: isProd ? '/oiva/oiva.github.io/' : '',
   images: {
     unoptimized: true,
